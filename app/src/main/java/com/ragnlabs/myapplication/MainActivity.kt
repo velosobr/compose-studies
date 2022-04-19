c5ba255675a8bdb336662873d1720d5b733d1f0b
Lino Veloso <lino.veloso@ifood.com.br>
2022-04-19 17:28:45 -0300

    Setting Up BizCard App

--- app/src/main/java/com/ragnlabs/myapplication/MainActivity.kt
+++ app/src/main/java/com/ragnlabs/myapplication/MainActivity.kt
@@ -3,15 +3,17 @@ package com.ragnlabs.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ragnlabs.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,9 +39,22 @@ fun Greeting(name: String) {
 
 @Composable
 fun CreateBizCard() {
-    Surface(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+
+            shape = RoundedCornerShape(corner = CornerSize(12.dp)),
+            elevation = 4.dp
+        ) {
+
+        }
 
 
     }
